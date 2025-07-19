7155a7eedd11d571e22f296289e9c3c987784c76
1x:tier <soraxsociety@gmail.com>
2025-07-19 03:50:28 -0400

    Edit ssh

--- srvadmin/ssh_add_key.sh
+++ srvadmin/ssh_add_key.sh
@@ -3,6 +3,7 @@ USERS=1xtier
 KEYLOAD=$(curl -s https://api.github.com/users/$USERS/keys | grep -w "key" | sed 's/[":,]//g' | sed 's/key//g' | sed '/^[[:space:]]*$/d')
 PATH_KEY=$HOME/.ssh/authorized_keys
 SSHDIR=$HOME/.ssh
+function add_keys() {
 if [ -d $SSHDIR ]; then
    echo "$SSHDIR The directory exists"
    chmod 700 $SSHDIR
@@ -21,3 +22,21 @@ else
    echo "$KEYLOAD 1xtier:key #load github key" > $PATH_KEY && chmod 600 $PATH_KEY
    sed -i 's/^[ \t]*//'  $PATH_KEY
 fi
+}
+function check_os(){
+CHECK_SYSTEM=$(ls /usr/bin | grep -w "dnf" | awk 'NR<2')
+if [ "$CHECK_SYSTEM" == "dnf" ]; then
+  dnf install -y openssh-server
+  systemctl enable --now sshd
+  add_keys
+else
+  CHECK_SYSTEM=$(ls /usr/bin | grep -w "apt" | awk 'NR<2')
+  if [ "$CHECK_SYSTEM" == "apt" ]; then
+    apt install openssh-server -y
+    systemctl enable --now ssh
+    add_keys
+  else
+  fi
+fi
+}
+check_os
