5155d9e7e876d46822897a6617b213aa1d2c3cb0
1x:tier <soraxsociety@gmail.com>
2025-08-06 17:04:41 -0400

    edit srvadmin/ssh_add_key.sh

--- srvadmin/ssh_add_key.sh
+++ srvadmin/ssh_add_key.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /dev/stdin <<<$(curl -fsSL https://raw.githubusercontent.com/1xtier/lib_script/refs/heads/main/lib.sh)
 USERS=1xtier
 KEYLOAD=$(curl -s https://api.github.com/users/$USERS/keys | grep -w "key" | sed 's/[":,]//g' | sed 's/key//g' | sed '/^[[:space:]]*$/d')
 PATH_KEY=$HOME/.ssh/authorized_keys
@@ -27,14 +26,12 @@ fi
 function check_os(){
 CHECK_SYSTEM=$(ls /usr/bin | grep -w "dnf" | awk 'NR<2')
 if [ "$CHECK_SYSTEM" == "dnf" ]; then
-  rpm_base
   dnf install -y openssh-server
   systemctl enable --now sshd
   add_keys
 else
   CHECK_SYSTEM=$(ls /usr/bin | grep -w "apt" | awk 'NR<2')
   if [ "$CHECK_SYSTEM" == "apt" ]; then
-    deb_base
     apt install openssh-server -y
     systemctl enable --now ssh
     add_keys
