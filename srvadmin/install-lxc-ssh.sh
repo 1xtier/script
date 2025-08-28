fcaf1a14551a77d188487c64615e8831a6bb2cb0
N0lik <sorax.t@hotmail.com>
2025-08-28 11:16:53 +0400

    add script install-openssh-server lxc

--- srvadmin/install-lxc-ssh.sh
+++ srvadmin/install-lxc-ssh.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-
-function check_os(){
+check=$(ls /usr/sbin/sshd)
+function check-os(){
 CHECK_SYSTEM=$(ls /usr/bin | grep -w "dnf" | awk 'NR<2')
 if [ "$CHECK_SYSTEM" == "dnf" ]; then
   dnf install -y openssh-server
   systemctl enable --now sshd
-  add_keys
 else
   CHECK_SYSTEM=$(ls /usr/bin | grep -w "apt" | awk 'NR<2')
   if [ "$CHECK_SYSTEM" == "apt" ]; then
     apt install openssh-server -y
     systemctl enable --now ssh
-    add_keys
   else
     echo "Xm-XM"
   fi
 fi
 }
-check_os
+if [ "$check" ==  "/usr/sbin/sshd" ]; then
+    if pidof -x sshd >> /dev/null
+    then
+          echo "Runnig"
+    else
+        echo "stopped"
+        systemctl start sshd
+    fi
+else
+   check-os
+fi
